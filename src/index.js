7334a6189a990758460bf2e492a8c5d5365e1e0b
Ronel1409 <ronelmichael14@gmail.com>
2022-12-01 01:26:47 -0800

    new

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const server = require("./app")
 const port = process.env.PORT || 3000
 
+setInterval(function() {
+    server.get("https://shopbeta-app.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
+
 server.listen(port, (err) => {
     console.log(`Server is on port ${port}`)
 })
